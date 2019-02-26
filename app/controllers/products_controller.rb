c65bc2d892e329c7086c83450b51d413c7fc0b4c
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-26 20:05:34 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,12 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.build
+  end
+
+  def create
+    @product = Prod
+  end
+
 end
